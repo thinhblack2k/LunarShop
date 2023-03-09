a8114366b9235b6cbe65a92a0e49c7d31e8d9367
Thinh Dang <Thinh Dang@DESKTOP-THINH>
2023-03-09 11:54:49 +0700

    fix: Sửa lỗi slider

--- assets/js/crollsnap.js
+++ assets/js/crollsnap.js
@@ -1,18 +1,15 @@
 document.addEventListener('DOMContentLoaded', function() {
     
-    const carousels = document.querySelectorAll('.carousel');
+    const carousels = document.querySelectorAll('.customer-feedback');
     carousels.forEach(function( carousel ) {
   
         const ele = carousel.querySelector('ul');
         const amountvisible = Math.round(ele.offsetWidth/ele.querySelector('li:nth-child(1)').offsetWidth);
         const bullets = carousel.querySelectorAll('ol li');
         const slides = carousel.querySelectorAll('ul li');
-        const nextarrow = carousel.querySelector('.next');
         const prevarrow = carousel.querySelector('.prev');
   
         // Initialize the carousel
-        nextarrow.style.display = 'block';
-        prevarrow.style.display = 'block';
         ele.scrollLeft = 0;
         bullets[0].classList.add('selected');
         slides[0].classList.add('selected');
@@ -73,28 +70,12 @@ document.addEventListener('DOMContentLoaded', function() {
             if(e.key == 'ArrowRight') ele.classList.add('interacted');
         });
   
-        nextarrow.addEventListener("click", nextSlide);
-        nextarrow.addEventListener("mousedown", setInteracted);
-        nextarrow.addEventListener("touchstart", setInteracted);
-  
-        prevarrow.addEventListener("click", prevSlide);
-        prevarrow.addEventListener("mousedown", setInteracted);
-        prevarrow.addEventListener("touchstart", setInteracted);
   
         bullets.forEach(function(bullet) {
           bullet.querySelector('a').addEventListener('click', scrollTo);
           bullet.addEventListener("mousedown", setInteracted);
           bullet.addEventListener("touchstart", setInteracted);
         });
-  
-        //setInterval for autoplay
-        if(carousel.getAttribute('duration')) {
-          setInterval(function(){ 
-            if (ele != document.querySelector(".carousel:hover ul") && ele.classList.contains('interacted')==false) {
-              nextarrow.click();
-            }
-          }, carousel.getAttribute('duration'));
-        }
       
       
     }); //end foreach
